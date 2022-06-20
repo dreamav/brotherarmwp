11c8c077205cb4d740c83b62f23d02457ab50b41
Mevericks <reloved@gmail.com>
2022-06-20 18:12:04 +0300

    почистил код в файлах

--- wp-content/themes/appsa/footer.php
+++ wp-content/themes/appsa/footer.php
@@ -1,6 +1,6 @@
+               
         <script type="text/javascript" src="<?bloginfo('template_url');?>/lib/jquery/jquery.min.js"></script>
         <!-- JavaScript -->
-               
         <?php wp_footer(); ?>
 
     </body>
--- wp-content/themes/appsa/page.php
+++ wp-content/themes/appsa/page.php
@@ -5,13 +5,12 @@ $scope->set_scope_var('current_url', get_page_link($page->ID));
 get_header();?>
     <body>
   
-        <div id="page" data-linkscroll='y'>
+        <div>
             <? get_template_part( 'parts/navigation' ); ?>
-<?
-echo do_shortcode($page->post_content);
-?>
-
-<? get_template_part( 'parts/footer' ); ?>
+			<?
+			echo do_shortcode($page->post_content);
+			?>
+			<? get_template_part( 'parts/footer' ); ?>
         </div>
 
 <?
