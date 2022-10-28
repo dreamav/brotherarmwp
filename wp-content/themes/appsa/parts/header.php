164c259215ff3aa0420b87af54d8a987aca7b104
Mevericks <reloved@gmail.com>
2022-10-28 17:52:49 +0300

    в хедере вывел перводимые для локали строки

--- wp-content/themes/appsa/parts/header.php
+++ wp-content/themes/appsa/parts/header.php
@@ -1,9 +1,15 @@
+<?
+global $blog_id;
+$current_blog_details = get_blog_details( array( 'blog_id' => $blog_id ) );
+$blogname = $current_blog_details->blogname;
+$lang_slug = $current_blog_details->path;
+?>
 <div id="header">
 
 	<div class="header_top wrap">
 		<div class="logo">
 			<img src="<?bloginfo('template_url');?>/images/top_logo.png">
-			<h1>ГО ПЛЕЧЕ ПОБРАТИМА</h1>
+			<h1><?=$blogname?></h1>
 		</div>
 
 		<ul class="header_menu">
@@ -18,10 +24,11 @@
 	<div class="header_center">
 		<div class="wrap">
 			<div class="plea">
-				<h1>ДОЛУЧАЙТЕСЬ ДО<br>НАШОЇ<br>ІНІЦІАТИВИ</h1>
+				<!-- <h1>ДОЛУЧАЙТЕСЬ ДО<br>НАШОЇ<br>ІНІЦІАТИВИ</h1> -->
+				<h1><?=sprintf( __( 'Join%sour%sinitiative', 'appsa' ), "<br>", "<br>" )?></h1>
 			</div>
 			<div class="button">
-				<a>допомогти</a>
+				<a><?=_e( 'Help us', 'appsa' )?></a>
 			</div>
 		</div>
 	</div>
