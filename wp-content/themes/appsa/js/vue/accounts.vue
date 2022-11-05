566520a668a7e269ceb10051fa0961c1358fda16
Mevericks <reloved@gmail.com>
2022-11-05 19:17:28 +0200

    німецька версія

--- wp-content/themes/appsa/js/vue/accounts.vue
+++ wp-content/themes/appsa/js/vue/accounts.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div id="card">
-			<h3>Bank Details</h3>
+			<h3>Bankdaten</h3>
 			 <tabs
 			 	:options="{ useUrlFragment: false }"
 			 >
@@ -25,7 +25,7 @@
 		    </tabs>			
 		</div>
 		<div id="crypto">
-			<h3>Crypto Wallets</h3>
+			<h3>Krypto-Brieftasche</h3>
 			<tabs
 			 	:options="{ useUrlFragment: false }"
 			 >
